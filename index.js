e052ead0707cda4ffc3f88ea56c2705e00e1030f
Odopascal <odopascal.po@gmail.com>
2024-05-02 17:15:39 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function opennav() {
+    document.getElementById("slidenav").style.width = "350px";
+    document.getElementById("menuicon").style.display = "none";
+}
+function closenav() {
+    document.getElementById("slidenav").style.width = "0";
+    document.getElementById("menuicon").style.display = "block";
+}
+// tabing section
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+       tabcontent[i].style.display = "none";
+    }
+    // Get all elements with class="tab" and remove the class "active"
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tablinks.length; i++) {
+       tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+   }
